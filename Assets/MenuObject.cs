6557806498c5f4c41e39c3d16f585072ae7ad89a
Alvaro Escurra <aescurra@MacBook-Pro-de-Alvaro.local>
2017-10-08 18:49:33 +0200

    first & completed task: create initial page, menus, & interativity with the pages

---
+++ Assets/MenuObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeScene(string name){
+		SceneManager.LoadScene (name);
+
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
