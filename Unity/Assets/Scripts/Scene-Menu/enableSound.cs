0c877f97b65a4479bcb59d6a9ddfb945bd0e26b9
TresLos <asistare7@gmail.com>
2017-12-20 15:50:49 +0100

    So fet i acabat

---
+++ Unity/Assets/Scripts/Scene-Menu/enableSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enableSound: MonoBehaviour {
+
+	public GameObject SoundButton;
+
+	public Sprite yesSound;
+	public Sprite nonSound;
+
+	private Image UIImage;
+
+	// Use this for initialization
+	void Start () {
+		UIImage = SoundButton.GetComponent<Image> ();
+
+		showSprite ();
+	}
+	
+	public void switchSound ()
+	{
+		Globals.enabledSound ^= true;
+		showSprite ();
+	}
+
+	private void showSprite ()
+	{
+		if (Globals.enabledSound)
+			UIImage.sprite = yesSound;
+		else
+			UIImage.sprite = nonSound;
+	}
+}
